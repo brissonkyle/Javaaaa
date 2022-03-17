74dab9870d780273256112f06f669b9070d2d4ca
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-03-16 21:09:47 -0600

    Started java

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var userAge = 18
+console.log('User is old enough');
+
+var is_subscribed = 'user is a sub'
+
+if (userAge >= 18) {
+    console.log('User is old enough');
+} else if (is_subscribed) {
+    console.log('Is a sub and old enough');
+}
+
